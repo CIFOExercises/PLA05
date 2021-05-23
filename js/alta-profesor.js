e02cb5f335fcd90e8eeed8584b986489aa652dc8
jmhs11 <josemariahs@gmail.com>
2021-05-23 13:31:31 +0200

    Start JavaScript

--- js/alta-profesor.js
+++ js/alta-profesor.js
@@ -17,21 +17,53 @@
 // Recoger la respuesta y evaluarla para mostrar una alerta con el resultado de la peticion
 
 let form = document.forms.namedItem('alta-profesor');
+form.onsubmit = handleRequest;
 
-form.onsubmit = sendRequest;
-
-function sendRequest(e) {
+function handleRequest(e) {
     e.preventDefault();
-    alert('Enviando peticion...')
+    
+    let isValid = validateForm();
+    
+    if(!isValid) return;
+  
+    let data = {
+      'nombre': form.firstName.value,
+      'usuario': form.user.value,
+      'email': form.email.value,
+      'password': form.password.value      
+    }
 
-    let name = form.firstName.value;
-    let user = form.user.value;
-    let email = form.email.value;
-    let password = form.password.value;
+    //Enviar Peticion al servidor: https://alcyon-it.com/PQTM/pqtm_alta_profesores.php
+    sendRequest(data);
+}
 
-    console.log(name, user, email, password)
+function validateForm(){
+    //Validar inputs
+    let validatedName = validateInput(form.firstName);
+    let validatedUser = validateInput(form.user);
+    let validatedEmail = validateInput(form.email);
+    let validatedPassword = validateInput(form.password);
+    
+    if (!validatedName || !validatedUser || !validatedEmail || !validatedPassword) return false;
+}
 
+function validateInput(input){
+	if (!input.value) {
+        console.log(input.value)
+		input.setCustomValidity('El campo debe estar informado');
+        return false;
+	} else {
+        if(input.type == 'email'){
+            let regex = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/g;
+            if(!input.value.match(regex)) input.input.setCustomValidity('El email no tiene el formato correcto');
+            console.log(regex.test(input.value))
+            return false;
+        }
+        input.setCustomValidity("");
+        return true;
+	}
+}
 
-    //Validar los campos antes de enviar
-
+function sendRequest({name, user, email, password}){
+    
 }
